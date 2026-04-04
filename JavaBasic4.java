394e11d5fba3e551f3b164ee77ddd91fcd140694
Om Bhadouriya <ombhadauria20@gmail.com>
2026-04-05 00:48:36 +0530

    FouthCommit

---
+++ JavaBasic4.java
@@ -0,0 +1,23 @@
+public class JavaBasic4 {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 20;
+        String name ="om bhadauria";
+
+        System.out.println("a");   
+         //we don't use the double quote because in double quote they give the answer that written in the double quote.
+         //as you can see that in the above i used double quote that answer me only a not giving the a value. 
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+
+        a = 25;
+        System.out.println(a);
+        //here we change the value of 'a' from 10 to 25. 
+
+        a = b;
+        System.out.println(a);
+        //here a is equal to b which means a is now 20.
+    }
+}
