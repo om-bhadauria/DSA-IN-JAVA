213bb02345977f20eaf7dff91b1d577018161011
Om Bhadouriya <ombhadauria20@gmail.com>
2026-04-05 01:12:37 +0530

    FifthCommit

---
+++ JavaBasic5.java
@@ -0,0 +1,34 @@
+public class JavaBasic5 {
+    public static void main(String args[]){
+
+                     //PRIMITIVE DATA TYPES
+
+        byte a = 25;          //byte range: -128 to +128
+        short b = 250;        //short range:-32,768 to +32,768
+        int c =2500;          //int range:-2,147,483,648 to +2,147,483,648  
+        long d = 2555555;     //long range:-9,223,372,036,854,775,808 to +9,223,372,036,854,775,808
+        float e = 10.45f;     //float range: STORE 6 TO 7 DECIMAL DIGIT.
+        double f = 10.045321; //double range: STORE 15 TO 16 DECIMAL DIGIT.
+        boolean g = true;     //boolean range : ONLY CONTAIN TRUE OR FALSE. 
+        char h = 'H';         //char range: ONLY CONTAIN SINGLE CHARACTER.
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+                    //NON-PRIMITIVE DATA TYPES
+        
+        String i ="om bhadauria";
+        //arrays
+        //classes
+        //interfaces
+        //object
+
+        System.out.println(i);
+    }
+}
