83c929d21fcbddb16e7b1c5656a69aa11631da44
Om Bhadouriya <ombhadauria20@gmail.com>
2026-04-05 00:21:55 +0530

    ThirdCommit

---
+++ JavaBasic3.java
@@ -0,0 +1,20 @@
+/*
+question 1: print this pattern 
+****
+***
+**
+*
+
+ */
+
+public class JavaBasic3 {
+    public static void main(String arg[]){
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
+
+// one more method 
+// System.out.println("****\n***\n**\n*")
