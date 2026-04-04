7e7da6bea4a12a12830297ec3696c9e3b463638e
Om Bhadouriya <ombhadauria20@gmail.com>
2026-04-04 18:55:09 +0530

    SecondCommit

---
+++ JavaBasic2.java
@@ -0,0 +1,10 @@
+public class JavaBasic2 {
+    public static void main(String args[]){
+        System.out.print("hello world");
+        System.out.println("hello world");
+         // 'ln' ki wajah se ek new line create hoti hai 
+         System.out.print("hello world \n"); 
+        // '/n' also used for creating new line 
+        System.out.print("hello world");
+    }
+}
