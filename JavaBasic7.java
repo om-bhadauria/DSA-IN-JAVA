7e19f55e45ade7d99903a5f86463eec04fe84314
Om Bhadouriya <ombhadauria20@gmail.com>
2026-04-07 17:58:23 +0530

    SeventhCommit

---
+++ JavaBasic7.java
@@ -0,0 +1,32 @@
+//INPUT IN JAVA
+
+import java.util.*;
+
+public class JavaBasic7 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+       // String input = sc.next(); 
+       // System.out.println(input);  
+       /*here we only use the sc,next that why they give only one sentence not the whole line  */
+
+        //String om = sc.nextLine();
+        //System.out.println(om);
+
+        int number =sc.nextInt();
+        System.out.println(number);
+
+        float price = sc.nextFloat();
+        System.out.println(price);
+
+        /*
+        sc.nextDouble()
+        sc.nextByte()
+        sc.nextShort()
+        sc.nextLong()
+        sc.nextBollean
+        v
+        
+        */
+    }
+    
+}
