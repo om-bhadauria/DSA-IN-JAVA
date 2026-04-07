c0e21900877b3df655f8ca01ed77478d4ae8f504
Om Bhadouriya <ombhadauria20@gmail.com>
2026-04-07 19:13:34 +0530

    TenthCommit

--- JavaBasic10.java
+++ JavaBasic10.java
@@ -1,3 +1,17 @@
 //find the area of the circle
 
+import java.util.*;
 
+public class JavaBasic10{
+    public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the radius of circle=");
+
+        double radius = sc.nextDouble();
+        double area = Math.PI*radius*radius;
+
+        System.out.println("area of the circle=" + area);
+    }
+    
+}
