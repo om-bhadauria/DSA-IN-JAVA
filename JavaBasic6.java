1e96d21dd1e0494209509ec31c96568fccb4d58c
Om Bhadouriya <ombhadauria20@gmail.com>
2026-04-07 17:38:13 +0530

    SixthCommit

---
+++ JavaBasic6.java
@@ -0,0 +1,8 @@
+public class JavaBasic6 {
+    public static void main(String arg[]){
+        int a = 25;
+        int b = 20;
+        int sum = a+b;
+        System.out.println("sum is " + sum);
+    }
+}
