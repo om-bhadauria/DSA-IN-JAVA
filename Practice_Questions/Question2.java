73ed6f35c9d7dcffabbc9ecc44d8b99f43504600
Om Bhadouriya <ombhadauria20@gmail.com>
2026-04-18 15:53:10 +0530

    Refactor Question2.java to improve code formatting and readability

--- Practice_Questions/Question2.java
+++ Practice_Questions/Question2.java
@@ -1,3 +1,17 @@
+//QUESTION 2) IN A PROGRAM, INPUT THE SIDE OF THE SQUARE . YOUI HAVE TO OUTPUT THE AREA OF THE SQUARE.
+// HINT(AREA OF SQUARE = SIDE * SIDE)
+
+package Practice_Questions;
+
+import java.util.*;
 public class Question2 {
-    
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int side = sc.nextInt();
+        System.out.println("ENTER THE SIDE OF THE SQUARE = " + side);
+
+        int area = side*side;
+        System.out.println("AREA OF THE SQUARE = " + area);
+}
 }
