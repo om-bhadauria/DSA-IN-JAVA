972e7dd18d4ad7115ee9b3f557f068885e20670d
Om Bhadouriya <ombhadauria20@gmail.com>
2026-04-22 23:29:54 +0530

    Add initial documentation for arithmetic operators in Operator1.java

---
+++ Operators/ArithmeticOperator/Operator1.java
@@ -0,0 +1,15 @@
+/*
+ARITHMETIC OPERATORS
+a) Binary Arithmetic Operators(2 operands)
+
+i)Addition Operator(+)
+ii)Subtraction Operator(-)
+iii)Multiplication Operator(*)
+iv)Division Operator(/)
+v)Modulus Operator(%) : they give the reminder of the division operation for eg) 5%2 = 1 
+
+
+
+b) Unary Arithmetic Operators(1 operand)
+c) Ternary Arithmetic Operators(3 operands)
+*/
