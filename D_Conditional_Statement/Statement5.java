144d729d65c65c6ecc4a510206baf95b37c76b2d
Om Bhadouriya <ombhadauria20@gmail.com>
2026-04-28 23:12:34 +0530

    Refactor Statement5.java to improve switch-case structure for better readability

--- D_Conditional_Statement/Statement5.java
+++ D_Conditional_Statement/Statement5.java
@@ -1,5 +1,19 @@
 package D_Conditional_Statement;
-
+import java.util.*;
 public class Statement5 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int button =sc.nextInt();
+
+        switch(button){
+            case 1:System.out.println("pizza");
+            break;
+            case 2:System.out.println("mango");
+            break;
+            case 3:System.out.println("burger");
+            break;
+            default : System.out.println("invalid button");
+        }
+    }
     
 }
