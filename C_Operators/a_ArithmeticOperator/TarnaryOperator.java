131a8538ebc8c657bcf239748b71c50f8ced658c
Om Bhadouriya <ombhadauria20@gmail.com>
2026-04-28 22:44:44 +0530

    Add TarnaryOperator.java to demonstrate the use of the ternary operator for even/odd number check

--- C_Operators/a_ArithmeticOperator/TarnaryOperator.java
+++ C_Operators/a_ArithmeticOperator/TarnaryOperator.java
@@ -1,5 +1,11 @@
 package C_Operators.a_ArithmeticOperator;
 
 public class TarnaryOperator {
+    public static void main(String[] args){
+        int number =40;
+
+        String type =((number%2)==0)? "even":"odd";
+        System.out.println(type);
+    }
     
 }
