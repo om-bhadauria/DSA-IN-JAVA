7dec290ff39156f3a6b5f3d0b371fe04218b509c
Om Bhadouriya <ombhadauria20@gmail.com>
2026-04-29 23:45:05 +0530

    Add Question4.java to evaluate values of x and y based on conditional statements

---
+++ C_Conditional_Statement/Practice_Question/Question4.java
@@ -0,0 +1,9 @@
+/*Question 4 :What will be the value of x & y in the following program :
+public class Solution{
+    public static void main(Stringargs[]){
+    int a = 63, b = 36;
+    boolean x = (a<b)?true:false; 
+    int y= (a>b)?a:b 
+*/
+
+//AMSWER : VALUE OF X = FALSE & Y =63
