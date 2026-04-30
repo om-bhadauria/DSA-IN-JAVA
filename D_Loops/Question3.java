43c6ef698201cc75ab97d37933c140d04a0c6ca6
Om Bhadouriya <ombhadauria20@gmail.com>
2026-04-30 21:46:06 +0530

    Add Question3.java to print the sum of the first n natural numbers based on user input

---
+++ D_Loops/Question3.java
@@ -0,0 +1,19 @@
+//Print sum of first n natural number 
+
+package D_Loops;
+import java.util.*;
+public class Question3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER:");
+        int n = sc.nextInt();
+        int sum =0;
+        int i =1;
+
+        while(i<=n){
+            System.out.println(sum=sum+i);
+            i ++;
+        }
+        sc.close();
+    }   
+}
