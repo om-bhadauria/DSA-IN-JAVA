c5b2cde1b8ddd7e0f621b3bd8e2aa271a19ae782
Om Bhadouriya <ombhadauria20@gmail.com>
2026-04-30 23:44:15 +0530

    Fix output formatting in Question5.java to print each digit on a new line

--- D_Loops/Question5.java
+++ D_Loops/Question5.java
@@ -2,6 +2,13 @@
 
 public class Question5 {
     public static void main(String[] args){
-        
+        int n =10899;
+
+        while(n > 0){
+            int lastdigit = n%10;
+            System.out.print(lastdigit + " ");
+            n = n/10;
+        }
+        System.out.println( );
     }
 }
