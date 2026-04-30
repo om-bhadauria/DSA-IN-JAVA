cf688790c9b578f130a6aa6043576cb91b541ad0
Om Bhadouriya <ombhadauria20@gmail.com>
2026-04-30 23:34:20 +0530

    Add Question4.java to print a square pattern using asterisks

---
+++ D_Loops/Question4.java
@@ -0,0 +1,16 @@
+/*
+PRINT THE SQUAR PATTERN
+****
+****
+****
+****
+ */
+
+public class Question4{
+    public static void main(String[] args){
+        for(int line=1 ;line<=4 ; line++){
+         System.out.println("****");
+            
+        }
+    }
+}
