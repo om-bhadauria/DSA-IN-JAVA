d6b7fdcd9baa45bf1474f2bf0bedef4184732139
Om Bhadouriya <ombhadauria20@gmail.com>
2026-04-30 17:39:33 +0530

    Add Question1.java and Types documentation for loop concepts

---
+++ D_Loops/Question1.java
@@ -0,0 +1,13 @@
+//Print the number from 1 to 10
+
+package D_Loops;
+
+public class Question1 {
+    public static void main(String[] args){
+    int number = 1;
+    while(number<=10){
+        System.out.println(number);
+        number ++ ;
+    }
+  }
+}
