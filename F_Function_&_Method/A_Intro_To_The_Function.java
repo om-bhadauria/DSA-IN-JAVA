076bbbab726de7d2733ced4df0f49736b2870a88
Om Bhadouriya <ombhadauria20@gmail.com>
2026-06-15 23:01:30 +0530

    Add initial implementation of function syntax and example in A_Intro_To_The_Function.java

---
+++ F_Function_&_Method/A_Intro_To_The_Function.java
@@ -0,0 +1,20 @@
+// FUNCTION : BLOCK OF CODE , REUSABLE PARTS 
+ 
+//SYNTAX of function
+
+/*
+returnType name(){
+    //body
+    return statement;
+}
+*/
+
+public class A_Intro_To_The_Function {
+    public static void printhelloworld(){
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args){
+        printhelloworld();
+    }
+}
